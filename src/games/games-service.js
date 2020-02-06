d9829e7e2ac01c1817ba8f2d23e9f42052074dd4
Patrick Gustafson <patrickgus@charter.net>
2020-02-06 20:02:04 +0000

    add insert, getById, delete, and update methods

--- src/games/games-service.js
+++ src/games/games-service.js
@@ -1,6 +1,32 @@
 const GamesService = {
   getAllGames(knex) {
     return knex.select("*").from("gamesnet_games");
+  },
+  insertGame(knex, newGame) {
+    return knex
+      .insert(newGame)
+      .into("gamesnet_games")
+      .returning("*")
+      .then(rows => {
+        return rows[0];
+      });
+  },
+  getById(knex, id) {
+    return knex
+      .from("gamesnet_games")
+      .select("*")
+      .where("id", id)
+      .first();
+  },
+  deleteGame(knex, id) {
+    return knex("gamesnet_games")
+      .where({ id })
+      .delete();
+  },
+  updateGame(knex, id, newGameFields) {
+    return knex("gamesnet_games")
+      .where({ id })
+      .update(newGameFields);
   }
 };
 
