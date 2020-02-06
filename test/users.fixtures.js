ed610780c5409bbb9b240060f527290fe358976e
Patrick Gustafson <patrickgus@charter.net>
2020-02-06 21:40:34 +0000

    change created to joined

--- test/users.fixtures.js
+++ test/users.fixtures.js
@@ -2,14 +2,14 @@ function makeUsersArray() {
   return [
     {
       id: 1,
-      date_created: "2029-01-22T16:28:32.615Z",
+      date_joined: "2029-01-22T16:28:32.615Z",
       fullname: "Sam Gamgee",
       username: "sam.gamgee",
       password: "secret"
     },
     {
       id: 2,
-      date_created: "2100-05-22T16:28:32.615Z",
+      date_joined: "2100-05-22T16:28:32.615Z",
       fullname: "Peregrin Took",
       username: "peregrin.took",
       password: "secret"
