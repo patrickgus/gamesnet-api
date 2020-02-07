da631a235ca8035df57dbc5f98178ccbfca43079
Patrick Gustafson <patrickgus@charter.net>
2020-02-07 17:50:42 +0000

    change DB_URL to DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://gamesnet-app.now.sh/",
-  DB_URL: process.env.DB_URL || "postgresql://patrick_gus@localhost/gamesnet",
-  TEST_DB_URL: process.env.TEST_DB_URL || "postgresql://patrick_gus@localhost/gamesnet_test"
+  DATABASE_URL: process.env.DATABASE_URL || "postgresql://patrick_gus@localhost/gamesnet",
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://patrick_gus@localhost/gamesnet_test"
 };
