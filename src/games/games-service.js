6c7e01ffebde72e58c152ae28d61c8fb1dd84da6
Patrick Gustafson <patrickgus@charter.net>
2020-02-07 14:36:38 +0000

    add getReviewsForGame method

--- src/games/games-service.js
+++ src/games/games-service.js
@@ -1,3 +1,5 @@
+const xss = require("xss");
+
 const GamesService = {
   getAllGames(knex) {
     return knex.select("*").from("gamesnet_games");
@@ -27,6 +29,66 @@ const GamesService = {
     return knex("gamesnet_games")
       .where({ id })
       .update(newGameFields);
+  },
+
+  getReviewsForGame(db, game_id) {
+    return db
+      .from("gamesnet_reviews AS rev")
+      .select(
+        "rev.id",
+        "rev.title",
+        "rev.rating",
+        "rev.review",
+        "rev.date_posted",
+        db.raw(
+          `json_strip_nulls(
+            row_to_json(
+              (SELECT tmp FROM (
+                SELECT
+                  usr.id,
+                  usr.username,
+                  usr.fullname,
+                  usr.date_joined
+              ) tmp)
+            )
+          ) AS "user"`
+        )
+      )
+      .where("rev.game_id", game_id)
+      .leftJoin("gamesnet_users AS usr", "rev.user_id", "usr.id")
+      .groupBy("rev.id", "usr.id");
+  },
+
+  serializeGame(game) {
+    return {
+      id: game.id,
+      title: xss(game.title),
+      cover: xss(game.cover),
+      avg_rating: game.avg_rating,
+      description: xss(game.description),
+      rated: xss(game.rated),
+      platforms: xss(game.platforms),
+      date_added: new Date(game.date_added),
+      poster_id: game.poster_id
+    };
+  },
+
+  serializeReview(review) {
+    const { user } = review;
+    return {
+      id: review.id,
+      game_id: review.game_id,
+      title: xss(review.title),
+      rating: review.rating,
+      review: xss(review.review),
+      date_posted: new Date(review.date_posted),
+      user: {
+        id: user.id,
+        fullname: user.fullname,
+        username: user.username,
+        date_joined: new Date(user.date_joined)
+      }
+    };
   }
 };
 
