88063790d6956eb9c9e0e8b4dcc8bc22e8e2a985
Patrick Gustafson <patrickgus@charter.net>
2020-02-07 15:57:16 +0000

    add CLIENT_ORIGIN for cors

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://gamesnet-app.now.sh/",
   DB_URL: process.env.DB_URL || "postgresql://patrick_gus@localhost/gamesnet",
   TEST_DB_URL: process.env.TEST_DB_URL || "postgresql://patrick_gus@localhost/gamesnet_test"
 };
