7191c30aa111a033e261452bbc4c14a4d449eb8c
Patrick Gustafson <patrickgus@charter.net>
2020-02-06 19:38:19 -0800

    add DB_URLs

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || "development"
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DB_URL: process.env.DB_URL || "postgresql://patrick_gus@localhost/gamesnet",
+  TEST_DB_URL: process.env.TEST_DB_URL || "postgresql://patrick_gus@localhost/gamesnet_test"
 };
