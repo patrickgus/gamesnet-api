681795a56bb4a733fb258e6a168450ca5d156780
Patrick Gustafson <patrickgus@charter.net>
2020-02-08 23:14:13 -0800

    delete unused endpoints

--- src/games/games-router.js
+++ src/games/games-router.js
@@ -1,29 +1,25 @@
 const path = require("path");
 const express = require("express");
 const GamesService = require("./games-service");
-const { requireAuth } = require('../middleware/jwt-auth')
+const { requireAuth } = require("../middleware/jwt-auth");
 
 const gamesRouter = express.Router();
 const jsonParser = express.json();
 
 gamesRouter
   .route("/")
-  .all(requireAuth)
+  // .all(requireAuth)
   .get((req, res, next) => {
-    const knexInstance = req.app.get("db");
-    GamesService.getAllGames(knexInstance)
+    GamesService.getAllGames(req.app.get("db"))
       .then(games => {
-        res.json(games.map(GamesService.serializeGame));
+        // console.log(games);
+        res.json(GamesService.serializeGames(games));
+        console.log(games);
       })
       .catch(next);
   })
   .post(jsonParser, (req, res, next) => {
-    const {
-      title,
-      description,
-      rated,
-      platforms
-    } = req.body;
+    const { title, description, rated, platforms } = req.body;
     const newGame = {
       title,
       description,
@@ -53,33 +49,8 @@ gamesRouter
   .route("/:game_id")
   .all(requireAuth)
   .all(checkGameExists)
-  .get((req, res, next) => {
+  .get((req, res) => {
     res.json(GamesService.serializeGame(res.game));
-  })
-  .delete((req, res, next) => {
-    GamesService.deleteGame(req.app.get("db"), req.params.game_id)
-      .then(numRowsAffected => {
-        res.status(204).end();
-      })
-      .catch(next);
-  })
-  .patch(jsonParser, (req, res, next) => {
-    const { title, description, rated, platforms } = req.body;
-    const gameToUpdate = { title, description, rated, platforms };
-
-    const numberOfValues = Object.values(gameToUpdate).filter(Boolean).length;
-    if (numberOfValues === 0)
-      return res.status(400).json({
-        error: {
-          message: `Request body must contain either 'title', 'description', 'rated', or 'platforms'`
-        }
-      });
-
-    GamesService.updateGame(req.app.get("db"), req.params.game_id, gameToUpdate)
-      .then(numRowsAffected => {
-        res.status(204).end();
-      })
-      .catch(next);
   });
 
 gamesRouter
@@ -89,7 +60,7 @@ gamesRouter
   .get((req, res, next) => {
     GamesService.getReviewsForGame(req.app.get("db"), req.params.game_id)
       .then(reviews => {
-        res.json(reviews.map(GamesService.serializeReview));
+        res.json(GamesService.serializeReviews(reviews));
       })
       .catch(next);
   });
