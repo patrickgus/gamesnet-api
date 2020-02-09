1e1f515f8ee985c2b5ff2ea2b856650f7503fb14
Patrick Gustafson <patrickgus@charter.net>
2020-02-08 22:09:31 -0800

    specify which characters for password

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -34,7 +34,7 @@ const UsersService = {
       return "Password must not start or end with empty spaces";
     }
     if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
-      return "Password must contain one upper case, lower case, number and special character";
+      return "Password must contain one upper case, lower case, number and special character(!,@,#,$,%,^,&)";
     }
     return null;
   },
