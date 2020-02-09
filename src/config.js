f7f5becf41c1a27a4edbe2ef6a01901e60d1ba41
Patrick Gustafson <patrickgus@charter.net>
2020-02-08 22:06:47 -0800

    add JWT secret and expiry time

--- src/config.js
+++ src/config.js
@@ -1,7 +1,12 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://gamesnet-app.now.sh/",
-  DATABASE_URL: process.env.DATABASE_URL || "postgresql://patrick_gus@localhost/gamesnet",
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://patrick_gus@localhost/gamesnet_test"
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://patrick_gus@localhost/gamesnet",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://patrick_gus@localhost/gamesnet_test",
+  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "3h"
 };
