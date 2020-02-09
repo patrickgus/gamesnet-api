fa051d8ef9645f085500c5b626724e7c75f7f7b0
Patrick Gustafson <patrickgus@charter.net>
2020-02-08 23:55:55 -0800

    refactor test-helper to include auth

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,25 +1,25 @@
 const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 function makeUsersArray() {
   return [
     {
       id: 1,
-      date_joined: new Date("2029-01-22T16:28:32.615Z"),
+      date_joined: "2029-01-22T16:28:32.615Z",
       fullname: "Test User 1",
       username: "test-user-1",
       password: "secret"
     },
     {
       id: 2,
-      date_joined: new Date("2100-05-22T16:28:32.615Z"),
+      date_joined: "2100-05-22T16:28:32.615Z",
       fullname: "Test user 2",
       username: "test-user-2",
       password: "secret"
     },
     {
       id: 3,
-      date_joined: new Date("2029-01-22T16:28:32.615Z"),
+      date_joined: "2029-01-22T16:28:32.615Z",
       fullname: "Test user 3",
       username: "test-user-3",
       password: "password"
@@ -37,8 +37,8 @@ function makeGamesArray(users) {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquet, odio vitae imperdiet finibus, purus nisi tincidunt lorem, feugiat bibendum mi mi sit amet urna. Donec a elementum nunc, sed dapibus augue. Duis volutpat at nisl eget varius. Nam vitae libero auctor, lacinia quam ac, fermentum dolor. Donec arcu neque, faucibus eu porta nec, laoreet sit amet eros.",
       rated: "M",
       platforms: "Playstaion",
-      date_added: new Date("2029-01-22T16:28:32.615Z"),
-      poster_id: users[1].id
+      date_added: "2029-01-22T16:28:32.615Z",
+      user_id: users[1].id
     },
     {
       id: 2,
@@ -48,8 +48,8 @@ function makeGamesArray(users) {
         "Sed pharetra, ante vel aliquet viverra, augue nunc malesuada dui, vel sollicitudin enim sem at urna. Integer pharetra sodales dolor, id accumsan purus aliquam sed. Donec at magna convallis, varius turpis in, elementum eros. Aliquam erat volutpat. Ut at leo eu ligula fermentum fringilla malesuada ut nisl.",
       rated: "T",
       platforms: "Playstation, Xbox, PC, Mobile",
-      date_added: new Date("2029-01-22T16:28:32.615Z"),
-      poster_id: users[0].id
+      date_added: "2029-01-22T16:28:32.615Z",
+      user_id: users[0].id
     },
     {
       id: 3,
@@ -59,8 +59,8 @@ function makeGamesArray(users) {
         "Mauris erat justo, facilisis eget lacus convallis, feugiat luctus massa. Nullam molestie ullamcorper nisl sed posuere. Aliquam erat volutpat. Morbi sed suscipit neque, eu fermentum dolor. Vivamus luctus, eros eu finibus auctor, arcu erat pretium sem, ut tempor mi quam vitae dolor. Mauris feugiat mi in imperdiet commodo.",
       rated: "M",
       platforms: "Xbox, Windows",
-      date_added: new Date("2029-01-22T16:28:32.615Z"),
-      poster_id: users[2].id
+      date_added: "2029-01-22T16:28:32.615Z",
+      user_id: users[2].id
     },
     {
       id: 4,
@@ -70,8 +70,8 @@ function makeGamesArray(users) {
         "In ut scelerisque leo, non ultricies velit. Nam ullamcorper tortor lorem, eu tincidunt turpis cursus porta. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer condimentum tempus ligula eget pretium. Duis id diam eu est egestas efficitur non consectetur ex.",
       rated: "E",
       platforms: "Playstation, Xbox, PC",
-      date_added: new Date("1919-12-22T16:28:32.615Z"),
-      poster_id: users[1].id
+      date_added: "1919-12-22T16:28:32.615Z",
+      user_id: users[1].id
     },
     {
       id: 5,
@@ -81,8 +81,8 @@ function makeGamesArray(users) {
         "Suspendisse viverra sodales metus tincidunt interdum. Phasellus nec tellus euismod, mollis turpis vel, scelerisque odio. Integer ante purus, lobortis a tincidunt sit amet, semper lobortis magna. Maecenas malesuada volutpat luctus. Praesent convallis metus id leo ornare porttitor non quis dui. Nunc in risus dignissim, iaculis lacus sed, cursus diam.",
       rated: "T",
       platforms: "Playstation, Xbox, PC, Switch, Stadia",
-      date_added: new Date("1919-12-22T16:28:32.615Z"),
-      poster_id: users[0].id
+      date_added: "1919-12-22T16:28:32.615Z",
+      user_id: users[0].id
     }
   ];
 }
@@ -96,7 +96,7 @@ function makeReviewsArray(users, games) {
       review: "This is the first test review",
       game_id: games[0].id,
       user_id: users[0].id,
-      date_posted: new Date("2029-01-22T16:28:32.615Z")
+      date_posted: "2029-01-22T16:28:32.615Z"
     },
     {
       id: 2,
@@ -105,7 +105,7 @@ function makeReviewsArray(users, games) {
       review: "This is the second test review",
       game_id: games[1].id,
       user_id: users[1].id,
-      date_posted: new Date("2029-01-22T16:28:32.615Z")
+      date_posted: "2029-01-22T16:28:32.615Z"
     },
     {
       id: 3,
@@ -114,7 +114,7 @@ function makeReviewsArray(users, games) {
       review: "This is the third test review",
       game_id: games[0].id,
       user_id: users[2].id,
-      date_posted: new Date("2029-01-22T16:28:32.615Z")
+      date_posted: "2029-01-22T16:28:32.615Z"
     },
     {
       id: 4,
@@ -123,24 +123,43 @@ function makeReviewsArray(users, games) {
       review: "This is the fourth test review",
       game_id: games[2].id,
       user_id: users[0].id,
-      date_posted: new Date("2029-01-22T16:28:32.615Z")
+      date_posted: "2029-01-22T16:28:32.615Z"
     }
   ];
 }
 
-function makeExpectedGame(users, game) {
-  const poster = users.find(user => user.id === game.poster_id);
+function makeExpectedGame(users, game, reviews = []) {
+  const user = users.find(user => user.id === game.user_id);
+
+  const gameReviews = reviews.filter(review => review.game_id === game.id);
+
+  const number_of_reviews = gameReviews.length;
+  const avg_rating = calculateAverageRating(gameReviews);
 
   return {
     id: game.id,
     title: game.title,
-    avg_rating: game.avg_rating,
     description: game.description,
     rated: game.rated,
     platforms: game.platforms,
-    date_added: game.date_added.toISOString(),
-    poster_id: game.poster_id
+    date_added: game.date_added,
+    number_of_reviews,
+    avg_rating,
+    user: {
+      id: user.id,
+      username: user.username,
+      fullname: user.fullname,
+      date_joined: user.date_joined
+    }
   };
+}
+
+function calculateAverageRating(reviews) {
+  if (!reviews.length) return 0;
+
+  const sum = reviews.map(review => review.rating).reduce((a, b) => a + b);
+
+  return Math.round(sum / reviews.length);
 }
 
 function makeExpectedGameReviews(users, gameId, reviews) {
@@ -153,12 +172,12 @@ function makeExpectedGameReviews(users, gameId, reviews) {
       title: review.title,
       rating: review.rating,
       review: review.review,
-      date_posted: review.date_posted.toISOString(),
+      date_posted: review.date_posted,
       user: {
         id: reviewUser.id,
         username: reviewUser.username,
         fullname: reviewUser.fullname,
-        date_joined: reviewUser.date_joined.toISOString()
+        date_joined: reviewUser.date_joined
       }
     };
   });
@@ -168,12 +187,11 @@ function makeMaliciousGame(user) {
   const maliciousGame = {
     id: 911,
     title: 'Naughty naughty very naughty <script>alert("xss");</script>',
-    avg_rating: 2,
     description: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
     rated: "U",
     platforms: "Xbox",
-    date_added: new Date(),
-    poster_id: user.id
+    date_added: new Date().toISOString(),
+    user_id: user.id
   };
   const expectedGame = {
     ...makeExpectedGame([user], maliciousGame),
@@ -232,7 +250,6 @@ function seedUsers(db, users) {
     .into("gamesnet_users")
     .insert(preppedUsers)
     .then(() =>
-      // update the auto sequence to stay in sync
       db.raw(`SELECT setval('gamesnet_users_id_seq', ?)`, [
         users[users.length - 1].id
       ])
@@ -264,13 +281,13 @@ function seedMaliciousGame(db, user, game) {
   );
 }
 
-// function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
-//   const token = jwt.sign({ user_id: user.id }, secret, {
-//     subject: user.user_name,
-//     algorithm: 'HS256',
-//   })
-//   return `Bearer ${token}`
-// }
+function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
+  const token = jwt.sign({ user_id: user.id }, secret, {
+    subject: user.user_name,
+    algorithm: "HS256"
+  });
+  return `Bearer ${token}`;
+}
 
 module.exports = {
   makeUsersArray,
@@ -283,5 +300,6 @@ module.exports = {
   cleanTables,
   seedUsers,
   seedGamesTables,
-  seedMaliciousGame
+  seedMaliciousGame,
+  makeAuthHeader
 };
