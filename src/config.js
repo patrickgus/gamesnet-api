2cc4af731caa84e12a690879cd889b5d97791a43
Patrick Gustafson <patrickgus@charter.net>
2020-02-12 22:50:21 -0800

    delete test database references

--- src/config.js
+++ src/config.js
@@ -3,10 +3,8 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://gamesnet-app.now.sh",
   DATABASE_URL:
-    process.env.DATABASE_URL || "postgres://qcwhksnjaeskst:0eafe89c6a29d3c6f8a0140a45ba46a9ff6a09ae41a627c2258cfa771b318c4d@ec2-52-203-160-194.compute-1.amazonaws.com:5432/d1si5dvtjj04tq",
-  TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL ||
-    "postgresql://patrick_gus@localhost/gamesnet_test",
+    process.env.DATABASE_URL ||
+    "postgres://qcwhksnjaeskst:0eafe89c6a29d3c6f8a0140a45ba46a9ff6a09ae41a627c2258cfa771b318c4d@ec2-52-203-160-194.compute-1.amazonaws.com:5432/d1si5dvtjj04tq",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "3h"
 };
